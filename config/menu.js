1fcbead2224bfc1f13677560f172bd193a2ed3be
OneDev <107590883+MINGYUknet@users.noreply.github.com>
2026-05-12 15:30:00 +0700

    Create menu.js

---
+++ config/menu.js
@@ -0,0 +1,8 @@
+// /src/config/menu.js
+export const menuItems = [
+  { name: "Home", id: "home" },
+  { name: "Gallery", id: "gallery" },
+  { name: "Video", id: "video" },
+  { name: "Social", id: "social" },
+  { name: "Contact", id: "contact" },
+];
